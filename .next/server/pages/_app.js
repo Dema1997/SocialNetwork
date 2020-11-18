723709237a374e88a859411d2d13e6de03b4e19f
Filippo De Marco <filippo@iMac-di-Admin.local>
2020-11-18 10:12:48 +0100

    fix: node_modules inside gitignore

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -121,7 +121,7 @@ function App({
 
 /***/ }),
 
-/***/ 7:
+/***/ 6:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__("1TCz");
--- .next/server/pages/api/secret.js
+++ .next/server/pages/api/secret.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 9);
+/******/ 	return __webpack_require__(__webpack_require__.s = 8);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -123,7 +123,7 @@ const KEY = 'abc';
 
 /***/ }),
 
-/***/ 9:
+/***/ 8:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__("+5R6");
